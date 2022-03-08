ce2a2c4dbf943ab25955df38de9bd92bec612d26
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-03-07 22:53:26 -0300

    Finish import categories

--- src/models/cars/useCases/category/importCategory/ImportCategoryService.ts
+++ src/models/cars/useCases/category/importCategory/ImportCategoryService.ts
@@ -1,11 +1,59 @@
+import fs from "fs";
+import { parse } from 'csv-parse';
+import { ICategoryRepository } from "../../../repositories/interfaces/ICategoryRepository";
 
+
+interface IImportCategory {
+    name: string,
+    description: string
+}
 
 class ImportCategoryService {
-    execute(file: Express.Multer.File): void {
+    constructor(private categoryRepository: ICategoryRepository) { }
 
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
 
+            const categories: IImportCategory[] = [];
+
+            const parseFile = parse();
+
+            stream.pipe(parseFile);
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description
+                    });
+                })
+                .on("end", () => {
+                    resolve(categories);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                });
+        });
     }
 
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const alreadyExists = this.categoryRepository.findCategory(name);
+
+            if (!alreadyExists) {
+                this.categoryRepository.create({
+                    name,
+                    description
+                });
+            }
+        });
+    }
 }
 
 export { ImportCategoryService };
--- src/models/cars/useCases/category/importCategory/Index.Category.ts
+++ src/models/cars/useCases/category/importCategory/Index.Category.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../../repositories/implementations/CategoriesRespository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryService } from "./ImportCategoryService";
 
-const importCategoryService = new ImportCategoryService();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryService = new ImportCategoryService(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryService);
 
 export { importCategoryController };
