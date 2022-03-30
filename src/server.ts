71922ac515eb210b86559f47d7f35a9b21ffce16
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-03-30 20:44:59 -0300

    Start documentation with Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerFile from "./swaggerFile.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationsRoutes);
 
 app.listen(3005, () => {
     console.log("Server is running on port 3005.");
+    console.log("See the documentation: http://localhost:3005/api-docs.");
 });
